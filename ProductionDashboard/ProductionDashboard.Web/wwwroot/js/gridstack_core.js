f378014543afd4c6610b6ed3013d29f9dd0fc2d8
WarOfDevil <vnktux@protonmail.com>
2020-06-21 01:43:56 +0200

    Add JQuery-UI plugin and add Module list menu structure

--- ProductionDashboard/ProductionDashboard.Web/wwwroot/js/gridstack_core.js
+++ ProductionDashboard/ProductionDashboard.Web/wwwroot/js/gridstack_core.js
@@ -2,7 +2,18 @@
     Created by WarOfDevil - 21/06/2020
 */
 
-var grid = GridStack.init({ float: true });
+var grid = GridStack.init({
+    alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    ),
+    resizable: {
+        handles: 'e, se, s, sw, w'
+    },
+    removable: '#trash',
+    removeTimeout: 100,
+    acceptWidgets: '.newWidget',
+    float: true
+});
 
 grid.on('added removed change', function (e, items) {
     var str = '';
@@ -10,24 +21,10 @@ grid.on('added removed change', function (e, items) {
     console.log(e.type + ' ' + items.length + ' items:' + str);
 });
 
-var items = [
-    { x: 2, y: 1, width: 1, height: 1 },
-    { x: 2, y: 3, width: 3, height: 1 },
-    { x: 4, y: 2, width: 1, height: 1 },
-    { x: 3, y: 1, width: 1, height: 2 },
-    { x: 0, y: 6, width: 2, height: 2 }
-];
-var count = 0;
-
-addNewWidget = function () {
-    var node = items[count] || {
-        x: Math.round(12 * Math.random()),
-        y: Math.round(5 * Math.random()),
-        width: Math.round(1 + 3 * Math.random()),
-        height: Math.round(1 + 3 * Math.random())
-    };
-    grid.addWidget('<div><div class="grid-stack-item-content">' + count++ + '</div></div>', node);
-};
-
-
-addNewWidget();
+// TODO: switch jquery-ui out
+$('.newWidget').draggable({
+    revert: 'invalid',
+    scroll: false,
+    appendTo: 'body',
+    helper: 'clone'
+});
